34bff00b0d421d0db6098d85cca4e44b253c6449
Zacky Rio Orlando <zackyrioorlando@gmail.com>
2026-03-09 10:26:20 +0700

    Jobsheet 4 Filament

--- app/Filament/Resources/Posts/Schemas/PostForm.php
+++ app/Filament/Resources/Posts/Schemas/PostForm.php
@@ -3,6 +3,16 @@
 namespace App\Filament\Resources\Posts\Schemas;
 
 use Filament\Schemas\Schema;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\ColorPicker;
+use Filament\Forms\Components\MarkdownEditor;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\TagsInput;
+use Filament\Forms\Components\Checkbox;
+use Filament\Forms\Components\DateTimePicker;
+use Illuminate\Validation\Rules\Unique;
 
 class PostForm
 {
@@ -11,6 +21,27 @@ class PostForm
         return $schema
             ->components([
                 //
+                TextInput::make('title')
+                ->minLength(5)
+                ->required(),
+                TextInput::make('slug')
+                ->unique()
+                ->required(),
+                select::make("category_id")
+                ->relationship("category", "name")
+                ->preload()
+                ->searchable(),
+                ColorPicker::make("color"),
+                MarkdownEditor::make("body"),
+                //RichEditor::make("content"),
+                FileUpload::make("image")
+                ->disk("public")
+                ->directory("posts")
+                ->visibility('public')
+                ->image(),
+                TagsInput::make("tags"),
+                Checkbox::make("published"),
+                DateTimePicker::make("published_at"),
             ]);
     }
 }
